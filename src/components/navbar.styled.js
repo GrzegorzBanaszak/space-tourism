7552781f7f4be50df02889611de579829dadebf9
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-05-05 18:07:26 +0200

    Correction for navbar

--- src/components/navbar.styled.js
+++ src/components/navbar.styled.js
@@ -8,10 +8,16 @@ export const NavContainer = styled.div`
   top: 0;
   left: 0;
   width: 100%;
+  @media (min-width: ${({ theme }) => theme.size.laptop}) {
+    padding: 2rem 0;
+  }
 `;
 
 export const NavLogo = styled.div`
   padding: 1rem;
+  @media (min-width: ${({ theme }) => theme.size.laptop}) {
+    padding-left: 3rem;
+  }
 `;
 
 export const NavHamburger = styled.div`
@@ -25,20 +31,41 @@ export const NavHamburger = styled.div`
 
 export const NavMenu = styled.div`
   display: none;
-  padding: 2rem;
   background-color: #97979744;
   a {
     margin-left: 1.2rem;
     text-decoration: none;
     text-transform: uppercase;
     color: white;
-    padding: 2rem 0;
-  }
-  a:hover {
-    border-bottom: 2px solid white;
+    padding: 3rem 0;
+    position: relative;
   }
   @media (min-width: ${({ theme }) => theme.size.mobileL}) {
-    display: block;
+    display: flex;
+    justify-content: space-around;
+    a:hover::after {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 2px;
+      background-color: #ccc;
+    }
+  }
+  @media (min-width: ${({ theme }) => theme.size.laptop}) {
+    position: relative;
+    width: 50%;
+    padding-right: 4rem;
+    &::before {
+      content: "";
+      position: absolute;
+      left: -39vw;
+      top: 50%;
+      width: 40vw;
+      height: 2px;
+      background-color: #97979744;
+    }
   }
 `;
 
@@ -60,7 +87,7 @@ export const NavMobileContainer = styled.div`
   flex-direction: column;
   margin-top: 6rem;
 
-  gap: 1.2rem;
+  gap: 2.5rem;
   a {
     margin-left: 1rem;
     text-decoration: none;
