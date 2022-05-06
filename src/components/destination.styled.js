e6fab5ac08d22d085526e3d839b3c5f8dc3ebbf7
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-05-06 09:56:22 +0200

    Add tablet and desktop media query

--- src/components/destination.styled.js
+++ src/components/destination.styled.js
@@ -28,27 +28,60 @@ export const DestinationHeader = styled.div`
     margin-right: 0.7rem;
     opacity: 0.4;
   }
+  @media (min-width: ${({ theme }) => theme.size.tablet}) {
+    padding-top: 10rem;
+    justify-content: start;
+  }
+  @media (min-width: ${({ theme }) => theme.size.laptop}) {
+    font-size: 1.3rem;
+    padding-top: 12rem;
+  }
 `;
 
 export const PlanetsContainer = styled.div`
   display: flex;
   flex-direction: column;
+  @media (min-width: ${({ theme }) => theme.size.laptop}) {
+    flex-direction: row;
+    margin-top: 10rem;
+  }
 `;
 
 export const PlanetImage = styled.div`
   margin: 3rem 2rem;
   img {
     width: 100%;
+    max-width: 350px;
+  }
+  @media (min-width: ${({ theme }) => theme.size.tablet}) {
+    margin: 3rem auto;
+  }
+
+  @media (min-width: ${({ theme }) => theme.size.laptop}) {
+    img {
+      max-width: 1200px;
+    }
   }
 `;
 
-export const PlanetContent = styled.div``;
+export const PlanetContent = styled.div`
+  @media (min-width: ${({ theme }) => theme.size.laptop}) {
+    max-width: 50%;
+  }
+`;
 
 export const PlanetList = styled.ul`
   list-style-type: none;
   display: flex;
   justify-content: space-between;
   margin-bottom: 1.7rem;
+  @media (min-width: ${({ theme }) => theme.size.tablet}) {
+    justify-content: center;
+    gap: 2rem;
+  }
+  @media (min-width: ${({ theme }) => theme.size.laptop}) {
+    justify-content: start;
+  }
 `;
 
 export const PlanetSelect = styled.li`
@@ -58,6 +91,9 @@ export const PlanetSelect = styled.li`
   letter-spacing: 2px;
   padding-bottom: 1rem;
   border-bottom: ${({ isSelect }) => (isSelect ? "2px solid white" : "none")};
+  &:hover {
+    border-bottom: 2px solid #ccc;
+  }
 `;
 
 export const PlanetName = styled.h2`
@@ -65,6 +101,9 @@ export const PlanetName = styled.h2`
   font-size: 5rem;
   text-transform: uppercase;
   margin-bottom: 0.5rem;
+  @media (min-width: ${({ theme }) => theme.size.laptop}) {
+    text-align: left;
+  }
 `;
 
 export const PlanetDescription = styled.p`
@@ -72,6 +111,14 @@ export const PlanetDescription = styled.p`
   font-size: 1.2rem;
   text-align: center;
   margin-bottom: 1rem;
+  line-height: 2rem;
+  @media (min-width: ${({ theme }) => theme.size.laptop}) {
+    text-align: left;
+    font-size: 1.5rem;
+    max-width: 70%;
+    margin-bottom: 5rem;
+    line-height: 2.5rem;
+  }
 `;
 
 export const PlanetInfo = styled.div`
@@ -81,11 +128,22 @@ export const PlanetInfo = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  @media (min-width: ${({ theme }) => theme.size.tablet}) {
+    flex-direction: row;
+    justify-content: space-evenly;
+  }
+  @media (min-width: ${({ theme }) => theme.size.laptop}) {
+    justify-content: start;
+    gap: 7rem;
+  }
   p {
     text-transform: uppercase;
     color: #d0d6f9;
     margin-top: 1rem;
     text-align: center;
+    @media (min-width: ${({ theme }) => theme.size.laptop}) {
+      text-align: left;
+    }
   }
 `;
 export const PlanetData = styled.div`
@@ -93,4 +151,7 @@ export const PlanetData = styled.div`
   padding-top: 1rem;
   font-size: 2rem;
   text-transform: uppercase;
+  @media (min-width: ${({ theme }) => theme.size.laptop}) {
+    text-align: left;
+  }
 `;
